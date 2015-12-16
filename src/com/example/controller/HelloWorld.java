0010d2634c034248de6538755e8a5ec3714710c0
suciuandrei <suciuandrei@users.noreply.github.com>
2015-12-16 16:21:48 +0200

    First commit

---
+++ src/com/example/controller/HelloWorld.java
@@ -0,0 +1,17 @@
+package com.example.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HelloWorld {
+ 
+	@RequestMapping("/welcome")
+	public ModelAndView helloWorld() {
+ 
+		String message = "<br><div style='text-align:center;'>"
+				+ "<h3>Hello World!</h3> </div><br><br>";
+		return new ModelAndView("welcome", "message", message);
+	}
+}
